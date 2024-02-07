3cb3d280a6c21e8ad0e797968af06a7c4b2a0766
koreanjys <connecttojys@gmail.com>
2024-02-07 13:00:32 +0900

    saying table updated

--- models/sayings.py
+++ models/sayings.py
@@ -16,69 +16,65 @@ class Saying(SQLModel, table=True):  # 명언 테이블 클래스
     id: Optional[int] = Field(default=None, primary_key=True)
 
     # 필수 필드
-    category: str
-    contents_kr: str
-    contents_detail: str
-
-    type_id: int = Field(default=0, nullable=False)  # 자동생성
-    created_at: Optional[datetime] = Field(default_factory=current_time_kst, nullable=False)
-    
+    category: str = Field(max_length=20, nullable=False)
+    type_id: int = Field(nullable=False)
+    content_desc_kr: str = Field(max_length=500, nullable=False)
+    useyn: int = Field(nullable=False)
 
     # 공백 가능 필드
-    url_name: Optional[str] = None
-    contents_eng: Optional[str] = None
-    contents_zh: Optional[str] = None
-    contents_divided: Optional[str] = None
-    author: Optional[str] = None
-    continent: Optional[str] = None
-
+    type_kr: Optional[str] = Field(default=None, max_length=50)
+    url_name: Optional[str] = Field(default=None, max_length=50)
+    type_chn: Optional[str] = Field(default=None, max_length=50)
+    content_desc_en: Optional[str] = Field(default=None, max_length=500)
+    author: Optional[str] = Field(default=None, max_length=50)
+    continent: Optional[str] = Field(default=None, max_length=20)
+    created_at: Optional[datetime] = Field(default_factory=current_time_kst)
     updated_at: Optional[datetime] = None
+
 
     # 모델 설정
     model_config = {
         "json_schema_extra": {
             "example": {
-                "url_name": "*출처(URL 혹은 블로그명)",
-                "contents_kr": "*명언 원본을 한국어로 작성",
-                "contents_detail": "*명언 원본을 해석",
-                "category": "명언 분류(예시: 인생)",
-                "contents_eng": "명언 원본을 영어로 작성",
-                "contents_zh": "명언 원본을 한자로 작성",
-                "contents_divided": "명언 해석을 한글로만 작성(특수문자 외국어 제거)",
+                "category": "예시: 인생",
+                "type_id": "0: 명언  1: 사자성어",
+                "type_chn": "사자성어(한자)",
                 "author": "저자",
-                "continent": "출처(지역)"
+                "continent": "출처(지역)",
+                "url_name": "출처(웹)",
+                "content_desc_kr": "내용(한글)",
+                "content_desc_en": "내용(영문)",
+                "useyn": "반영 여부 0: 반영  1: 미반영"
             }
         }
     }
 
 
 class SayingUpdate(SQLModel):
-    url_name: Optional[str] = None
-    contents_kr: Optional[str] = None
-    contents_detail: Optional[str] = None
-    type_id: Optional[str] = None
     category: Optional[str] = None
-    contents_eng: Optional[str] = None
-    contents_zh: Optional[str] = None
-    contents_divided: Optional[str] = None
+    type_id: Optional[int] = None
+    content_desc_kr: Optional[str] = None
+    useyn: Optional[int] = None
+    type_kr: Optional[str] = None
+    url_name: Optional[str] = None
+    type_chn: Optional[str] = None
+    content_desc_en: Optional[str] = None
     author: Optional[str] = None
     continent: Optional[str] = None
-    # updated_at: datetime = Field(default_factory=current_time_kst, nullable=False)  # 여기서 설정해도 적용이 안되므로 router에서 직접 값을 추가
 
     # 모델 설정
     model_config = {
         "json_schema_extra": {
             "example": {
-                "url_name": "출처(URL 혹은 블로그명)",
-                "contents_kr": "명언 원본을 한국어로 작성",
-                "contents_detail": "명언 원본을 해석",
+                "category": "예시: 인생",
                 "type_id": "0: 명언  1: 사자성어",
-                "category": "명언 분류(예시: 인생)",
-                "contents_eng": "명언 원본을 영어로 작성",
-                "contents_zh": "명언 원본을 한자로 작성",
-                "contents_divided": "명언 해석을 한글로만 작성(특수문자 외국어 제거)",
+                "type_chn": "사자성어(한자)",
                 "author": "저자",
-                "continent": "출처(지역)"
+                "continent": "출처(지역)",
+                "url_name": "출처(웹)",
+                "content_desc_kr": "내용(한글)",
+                "content_desc_en": "내용(영문)",
+                "useyn": "반영 여부 0: 반영  1: 미반영"
             }
         }
     }
--- routes/sayings.py
+++ routes/sayings.py
@@ -16,17 +16,17 @@ saying_router = APIRouter(tags=["Sayings"])
 @saying_router.get("/", response_model=List[Saying])
 async def retrieve_all_sayings(session=Depends(get_session)) -> List[Saying]:
     """
-    저장된 모든 명언들 조회
+    저장된 모든 데이터들 조회
     """
     statement = select(Saying)
-    sayings = session.exec(statement).all()  # 명언 테이블의 모든 값을 sayings에 리스트로 불러옴
+    sayings = session.exec(statement).all()  # 데이터 테이블의 모든 값을 sayings에 리스트로 불러옴
     return sayings
 
 
 @saying_router.get("/{id}", response_model=Saying)
 async def retrieve_saying(id: int, session=Depends(get_session)) -> Saying:
     """
-    명언 조회
+    데이터 조회
     """
     saying = session.get(Saying, id)
     if saying:
@@ -34,14 +34,14 @@ async def retrieve_saying(id: int, session=Depends(get_session)) -> Saying:
     
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail="선택한 ID를 가진 명언이 존재하지 않습니다."
+        detail="선택한 ID를 가진 데이터이 존재하지 않습니다."
     )
 
 
 @saying_router.post("/new", response_model=Saying)
 async def create_new_saying(new_saying: Saying, session=Depends(get_session)) -> Saying:
     """
-    명언 새로 생성
+    데이터 새로 생성
     """
     session.add(new_saying)
     session.commit()
@@ -52,7 +52,7 @@ async def create_new_saying(new_saying: Saying, session=Depends(get_session)) ->
 @saying_router.put("/edit/{id}", response_model=Saying)
 async def update_saying(id: int, new_data: SayingUpdate, session=Depends(get_session)) -> Saying:
     """
-    명언 수정
+    데이터 수정
     """
     saying = session.get(Saying, id)
     if saying:
@@ -67,23 +67,23 @@ async def update_saying(id: int, new_data: SayingUpdate, session=Depends(get_ses
     
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail="선택한 ID를 가진 명언이 존재하지 않습니다."
+        detail="선택한 ID를 가진 데이터이 존재하지 않습니다."
     )
 
 @saying_router.delete("/delete/{id}")
 async def delete_saying(id: int, session=Depends(get_session)) -> dict:
     """
-    명언 삭제
+    데이터 삭제
     """
     saying = session.get(Saying, id)
     if saying:
         session.delete(saying)
         session.commit()
         return {
-            "message": "명언을 삭제했습니다."
+            "message": "데이터을 삭제했습니다."
         }
     
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail="선택한 ID를 가진 명언이 존재하지 않습니다."
+        detail="선택한 ID를 가진 데이터가 존재하지 않습니다."
     )
